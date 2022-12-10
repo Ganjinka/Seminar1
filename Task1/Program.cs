ad5f5d0e0c2391f4d6dcbb36d94b60b599968dc1
Ganjinka <ganjinka.gm@gmail.com>
2022-12-11 00:52:27 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Vvedite pervoe chislo:");
+int firstN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite vtoroe chislo:");
+int secondN = Convert.ToInt32(Console.ReadLine());
+if (firstN > secondN) {
+    Console.WriteLine(firstN + " > " + secondN);
+} else if (firstN == secondN) {
+    Console.WriteLine(firstN + " = " + secondN);
+} else {
+    Console.WriteLine(secondN + " > " + firstN);
+}
