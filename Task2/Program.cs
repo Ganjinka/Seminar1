3e86a4752150e294f6378e5813b52281b0e26743
Ganjinka <ganjinka.gm@gmail.com>
2022-12-11 21:41:47 +0500

    Added Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Vvedite pervoe chislo:");
+int firstN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite vtoroe chislo:");
+int secondN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Vvedite trete chislo:");
+int thirdN = Convert.ToInt32(Console.ReadLine());
+int bolsheeChislo = firstN;
+if (firstN > secondN) {
+    bolsheeChislo = firstN;
+} else if (firstN == secondN) {
+    bolsheeChislo = firstN;
+} else if (firstN < secondN) {
+    bolsheeChislo = secondN;
+}
+if (bolsheeChislo > thirdN) {
+ bolsheeChislo = firstN; 
+} else if (bolsheeChislo < thirdN) {
+    bolsheeChislo = thirdN;
+}
+Console.WriteLine("Samoe bolshoe chislo: " + bolsheeChislo);
+
+
+
+
